b11c5997ef37951e69698b74781dad97a2bef2a3
eujenio21 <epifaniosantos118@gmail.com>
2025-05-26 09:25:34 -0300

    Update page.js

--- app/(landing)/page.js
+++ app/(landing)/page.js
@@ -1,83 +1,37 @@
 import Image from "next/image";
 
-import Header from "./_components/Header";
-import Nav from "./_components/Nav";
-import Hero from "./_components/Hero";
-import About from "./_components/About";
-import Tokenomics from "./_components/Tokenomics";
-import Exchanges from "./_components/Exchanges";
-import HowToBuy from "./_components/HowToBuy";
-import WallOfLove from "./_components/WallOfLove";
-import Footer from "./_components/Footer";
-
 export default function Home() {
   return (
-    <>
-      <Header>
-        <Nav />
-      </Header>
-      <Hero />
-      <div className="bg-[#e4e0e1]">
-        <About />
-        <Exchanges />
-        <div className="max-w-[85rem] mx-auto mt-24">
-          <span className="flex items-center">
-            <span className="h-[3px] flex-1 bg-duis"></span>
-            <div>
-              <Image
-                className=""
-                src="/collection/5.png"
-                width={300}
-                height={300}
-                alt="dui-5"
-              />
-            </div>
-            <span className="h-[3px] flex-1 bg-duis"></span>
-          </span>
-        </div>
+    <main style={{ textAlign: "center", padding: "20px" }}>
+      <h1>🚀 Airdrop do GAT está AO VIVO!</h1>
+      <p>Receba tokens grátis do memecoin mais estiloso do universo 😎</p>
 
-        <Tokenomics />
-        <div className="max-w-[85rem] mx-auto mt-24">
-          <span className="flex items-center">
-            <span className="h-[3px] flex-1 bg-duis"></span>
-            <div>
-              <Image
-                className=""
-                src="/collection/7.png"
-                width={300}
-                height={300}
-                alt="dui-7"
-              />
-            </div>
-            <span className="h-[3px] flex-1 bg-duis"></span>
-          </span>
-        </div>
+      <Image
+        src="/gat-mascote.png"
+        alt="Mascote GAT"
+        width={300}
+        height={300}
+      />
 
-        <HowToBuy />
-        <div className="text-center">
-          <div className="flex justify-center">
-            <Image
-              className="hidden md:block"
-              src="/collection/6.png"
-              width={300}
-              height={300}
-              alt="dui-6"
-            />
-            <Image
-              className=""
-              src="/collection/8.png"
-              width={300}
-              height={300}
-              alt="dui-8"
-            />
-          </div>
-          <div className="mt-5">
-            <p className="font-stopbuck text-2xl text-black">Woof! Woof!</p>
-          </div>
-        </div>
-        {/* <WallOfLove /> */}
-        <Footer />
-      </div>
-    </>
+      <br /><br />
+
+      <a
+        href="https://twitter.com/intent/tweet?text=Participe+do+airdrop+do+GAT!+gat-coin.vercel.app"
+        target="_blank"
+      >
+        <button
+          style={{
+            backgroundColor: "#f7931a",
+            color: "white",
+            padding: "12px 24px",
+            borderRadius: "10px",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+        >
+          📣 Compartilhar no Twitter
+        </button>
+      </a>
+    </main>
   );
 }
